5979184c3de6d596fcdfc31c0749e9ddeb4aaace
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-24 14:09:56 +0300

    fix: fix drawCircle function

--- src/utils/drawCircle.ts
+++ src/utils/drawCircle.ts
@@ -2,12 +2,9 @@ import { getMousePos, mouseToggle, moveMouse } from "robotjs";
 
 export function drawCircle(radius: number) {
   const mousePos = getMousePos();
-
-  for (let i = 0; i <= Math.PI * 2; i += 0.05) {
-    // Convert polar coordinates to cartesian
-    const x = mousePos.x + radius * Math.cos(i);
+  for (let i = 0; i <= Math.PI * 2 + 0.05; i += 0.05) {
+    const x = mousePos.x + radius * Math.cos(i) - radius;
     const y = mousePos.y + radius * Math.sin(i);
-
     moveMouse(x, y);
     mouseToggle("down");
   }
