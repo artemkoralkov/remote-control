b449213c012f015cb081886cd4f5dfc3d7c24e94
Koralkov Artem <artemkoralkov@gmail.com>
2022-06-24 20:25:09 +0300

    refactor: rewtrite httpServer with stream api

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -6,13 +6,12 @@ export const httpServer = http.createServer(function (req, res) {
   const __dirname = path.resolve(path.dirname(""));
   const file_path =
     __dirname + (req.url === "/" ? "/front/index.html" : "/front" + req.url);
-  fs.readFile(file_path, function (err, data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-    res.writeHead(200);
-    res.end(data);
+  const front = fs.createReadStream(file_path);
+  res.writeHead(200);
+  front.pipe(res);
+  front.on("error", (error) => {
+    res.writeHead(404);
+    res.end(JSON.stringify(error));
+    return;
   });
 });
